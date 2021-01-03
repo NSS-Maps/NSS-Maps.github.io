23e50a1201ba06d96dab960ffc284a6dc64d7601
Andy Collins <askingalot@gmail.com>
2021-01-03 15:48:09 -0600

    refactor build script

--- .github/scripts/build_html.py
+++ .github/scripts/build_html.py
@@ -2,21 +2,21 @@
 import os
 import markdown2
 
-ROOT_DIR = './'
-MAPS_DIR = ROOT_DIR + 'maps'
-TMPL_PATH = ROOT_DIR + '.github/scripts/index.tmpl.html'
-INDEX_PATH = ROOT_DIR + 'index.html'
+
+ROOT_DIR = "./"
+MAPS_DIR = ROOT_DIR + "maps"
+TMPL_PATH = ROOT_DIR + ".github/scripts/index.tmpl.html"
+INDEX_PATH = ROOT_DIR + "index.html"
 
 
 def main():
-    with open(TMPL_PATH, 'r') as tmpl:
+    with open(TMPL_PATH, "r") as tmpl:
         template = tmpl.read()
-    
-    map_data = scan_maps_dir(MAPS_DIR)
-    html_content = map_data_to_html(map_data)
 
-    with open(INDEX_PATH, 'w') as output:
-        output.write(template.format(content = html_content))
+    html_content = map_data_to_html(scan_maps_dir(MAPS_DIR))
+
+    with open(INDEX_PATH, "w") as output:
+        output.write(template.format(content=html_content))
 
 
 def scan_maps_dir(path):
@@ -24,66 +24,71 @@ def scan_maps_dir(path):
     markdown_entries = []
 
     for entry in os.scandir(path):
-        if not (entry.is_dir() or entry.name.endswith('.html') or entry.name.endswith('.md')):
-            continue
-
-        if entry.name.endswith('.md'):
+        if entry.name.endswith(".md"):
             markdown_entries.append(entry)
             continue
 
+        if not (entry.is_dir() or entry.name.endswith(".html")):
+            continue
+
         results[entry.name] = {
-            'name': entry.name,
-            'path': entry.path,
-            'description': '',
-            'children': {}
+            "name": os.path.splitext(entry.name)[0],
+            "path": entry.path,
+            "description": "",
+            "children": scan_maps_dir(entry.path) if entry.is_dir() else {},
         }
 
-        if entry.is_dir():
-            results[entry.name]['children'] = scan_maps_dir(entry.path)
-
-    for md_entry in markdown_entries:
-        base_name = md_entry.name[:-3]
-        html_name = base_name + '.html'
-
-        if base_name in results or html_name in results:
-            with open(ROOT_DIR + md_entry.path, 'r') as md_input:
-                description = md_input.read()
-            
-            if base_name in results:
-                results[base_name]['description'] = description
-            else:
-                results[html_name]['description'] = description
-
+    attach_descriptions(markdown_entries, results)
     return results
 
 
-def map_data_to_html(map_data):
-    html = ''
-    for name in sorted(map_data):
-        entry = map_data[name]
+def attach_descriptions(markdown_entries, map_entries):
+    for md_entry in markdown_entries:
+        base_name = os.path.splitext(md_entry.name)[0]
+        html_name = base_name + ".html"
 
-        if entry['children']:
-            html += f'''
+        if base_name in map_entries:
+            entry_key = base_name
+        elif html_name in map_entries:
+            entry_key = html_name
+        else:
+            continue
+
+        with open(ROOT_DIR + md_entry.path, "r") as md_input:
+            map_entries[entry_key]["description"] = md_input.read()
+
+
+def map_data_to_html(map_entries):
+    html = ""
+
+    for key in sorted(map_entries):
+        entry = map_entries[key]
+
+        if entry["children"]:
+            html += f"""
                 <section class="collection">
                     <header class="collection__header">{entry['name']}</header>
-                    {entry['description'] and f"""
-                        <div class="description">
-                            {markdown2.markdown(entry['description'])}
-                        </div>"""}
+                    {description_html(entry)}
                     {map_data_to_html(entry['children'])}
-                </section>
-            '''
+                </section>"""
         else:
-            html += f'''
+            html += f"""
                 <div class="card">
                     <a href="{entry['path']}" target="_new">{entry['name']}</a>
-                    {entry['description'] and f"""
-                        <div class="description">
-                            {markdown2.markdown(entry['description'])}
-                        </div>"""}
-                </div>
-            '''
+                    {description_html(entry)}
+                </div>"""
+
     return html
+
+
+def description_html(entry):
+    if not entry["description"]:
+        return ""
+
+    return f"""
+        <div class="description"> 
+            {markdown2.markdown(entry["description"])} 
+        </div>"""
 
 
 main()
